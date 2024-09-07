e8bdc738914472254d6a4e3dfafb0004cc40b1cc
Vedant794 <vedantkumbhare721@gmail.com>
2024-09-07 10:58:52 +0530

    Initial Setup of React+TypeScript done

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
